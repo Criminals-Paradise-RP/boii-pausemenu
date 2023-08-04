061469fa2e5c66d4d1ab76917e79997ba9879448
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-08-04 16:50:26 -0700

    Update config_sv.lua
    Signed-off-by: JordanBScott <99845881+JordanBScott@users.noreply.github.com>

--- server/config_sv.lua
+++ server/config_sv.lua
@@ -6,16 +6,16 @@ Config = {}
 -- Discord settings
 Config.Discord = {
     -- Discord webhooks refer to; https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks
-    Webhook = 'YOUR_DISCORD_WEBHOOK',
+    Webhook = 'https://discord.com/api/webhooks/1137161339509424298/yLX6tMVtxs5dxjaFN5czVOWJsgU_vWiJhDsreiKsQ7Uz0YEgm17eH88DQ0MkNPAz_Nvk',
     -- Webhook colour can find more here; -- https://www.spycolor.com/
-    Colour = 5098434, -- boii development brand blue 
+    Colour = 3716863, -- boii development brand blue 5098434
     -- Discord bot username;
     BotData = {
-        Name = '-🛡️- BOII REPORTS -🛡️-',
-        Logo = 'https://i.imgur.com/3g0XonA.png',
+        Name = '-🛡️- CPRP REPORTS -🛡️-',
+        Logo = 'https://cdn.discordapp.com/attachments/967905287673618512/1135060840391909446/NoBackgroundOrigional.png',
         Footer = {
-            Text = '© - BOII | Development ',
-            Icon = 'https://i.imgur.com/3g0XonA.png'
+            Text = '© - CPRP | Development ',
+            Icon = 'https://cdn.discordapp.com/attachments/967905287673618512/1135060840391909446/NoBackgroundOrigional.png'
         },
     }
 }
